cc996a27525e95c2336a2d8a3f3e66c76c26d462
Visionluxx <honhatquang247@gmail.com>
2025-02-02 14:00:41 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 
 app = Flask(__name__, template_folder="template")
-
+app.config["SECRET_KEY"]="jejdfbdjdi"
 
 @app.route('/', methods=["POST", "GET"])
 def loginAndSave():
     if request.method=="POST":
         userName=request.form["userName"]
         password=request.form["password"]
+        session["name"]=userName
+        session["password"]=password
         f=open("data.txt","a")
         f.write(userName)
         f.write("   pass is:")
@@ -17,9 +19,12 @@ def loginAndSave():
 
 @app.route("/data")
 def data():
-    file_path = "/opt/render/project/src/data.txt"
-    with open(file_path, "r") as f:
-        content = f.read()
+    if session["name"]=="admin" and session["password"]=="quang123":
+        file_path = "/opt/render/project/src/data.txt"
+        with open(file_path, "r") as f:
+            content = f.read()
+    else:
+         content="permission denied"
     return content
 
 if __name__ == '__main__':
